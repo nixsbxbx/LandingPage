9639e0fbd008f1ac811d8f3922081ce29220f42c
Denisse <denissehernandezvazquez793@gmail.com>
2026-02-06 14:52:37 -0600

    segundo commit

---
+++ src/components/header.tsx
@@ -0,0 +1,22 @@
+export const Header = () => {
+  return (
+    <header>
+      <a href="/">
+        <img src="/images/logo.svg" alt="Logo" />
+      </a>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Features</a>
+          </li>
+          <li>
+            <a href="#">Team</a>
+          </li>
+          <li>
+            <a href="#">Sing In</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
