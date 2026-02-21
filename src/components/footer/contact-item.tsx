a7b61b671a6e824f2730921b24c4e859de27fe87
Denisse <denissehernandezvazquez793@gmail.com>
2026-02-20 23:42:52 -0600

    4to

--- src/components/footer/contact-item.tsx
+++ src/components/footer/contact-item.tsx
@@ -1,14 +1,14 @@
 interface Props {
     text: string,
     inconUrl: string;
-    alt : string;
+    alt: string;
     styles?: string;
 }
 
-export const ContactItem = ({text, inconUrl, alt, styles=''} : Props) => {
+export const ContactItem = ({text, inconUrl, alt, styles=''}: Props) => {
     return (
-        <div className={"flex items-center gap-6 ${styles}"}>
-            <img className="mt-1" src={inconUrl} alt="{alt}" />
+        <div className={`flex items-start gap-6 ${styles}`}>
+            <img className='mt-1' src={inconUrl} alt={alt} />
             <p>{text}</p>
         </div>
     )
